bc95e13b1d9c69882147a8e67d494ce8f25d6764
King-Ryann <79991190+King-Ryann@users.noreply.github.com>
2021-04-13 15:05:08 +0800

    Update puzzlescript.js

--- puzzlescript.js
+++ puzzlescript.js
@@ -1,7 +1,7 @@
 
   function showTopic(){
   var response = prompt("请切换至英文输入法模式输入完整等式");
-  if (response=="7-(7/7)=6")
+  if (response=="7-7/7=6")
     {alert("🐂");}
   else
     {alert("？？？不对再猜") }
